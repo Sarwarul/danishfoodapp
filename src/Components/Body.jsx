43b57fdc8cf591f07405e648bb8c90afefd40211
Sarwarul <sarwarulsheikh1508@gmail.com>
2023-08-08 17:16:02 +0530

    Searchbutton added

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import Hotellist from "./Hotellist";
 import "./Body.css";
+
+
+
+
+
+function filterData(searchTxt,Hotel){
+
+const filterData=Hotel.filter((Hotel)=>
+Hotel.name.includes(searchTxt)
+);
+return filterData;
+}
 const Body=()=>{
-    console.log("Body",Hotellist);
+// const searchTxt="KFC";
+const[searchTxt,setsearchTxt]=useState("KFC");
+const[Hotel,setHotel]=useState(Hotellist);
   return(
+    <>
+    <div className="search-container">
+     
+       <input type="text" className="searchinput" placeholder="search"  value={searchTxt}
+       onChange={(e)=>{
+        setsearchTxt(e.target.value);
+       }}/>
+       
+<button className="search-btn" onClick={()=>{
+
+// need to filter data
+const data= filterData(searchTxt,Hotel);
+setHotel(data);
+// update the state-hotel varible
+// filterData
+}}>search</button>
+    </div>
    <div className="Body-list">
-    <Card Hotellist={Hotellist[0].name}/>
+    {
+      Hotel.map((Hotel)=>{
+        return <Card{...Hotel} key={Hotel.id}/>;
+      })
+      
+      /* <Card {...Hotellist[0]}/> */}
+    {/* <Card {...Hotellist[1]}/> */}
+    {/* // <Card name={Hotellist[2].name} cuisines={Hotellist[2].cuisines}/> */}
+    {/* // <Card name={Hotellist[3].name} cuisines={Hotellist[3].cuisines}/>    <Card name={Hotellist[5].name} cuisines={Hotellist[5].cuisines}/> */}
+    {/* // <Card name={Hotellist[6].name} cuisines={Hotellist[6].cuisines}/> */}
+    {/* // <Card name={Hotellist[7].name} cuisines={Hotellist[7].cuisines}/> */}
+    {/* // <Card name={Hotellist[8].name} cuisines={Hotellist[8].cuisines}/> */}
+    {/* // <Card name={Hotellist[9].name} cuisines={Hotellist[9].cuisines}/> */}
+    {/* // <Card={Hotellist[10].name} cuisines={Hotellist[10].cuisine */}
     
-    <Card/>
-    <Card/>
-    <Card/>
+    
+
+
    </div>
+   </>
   );
 }
 export default Body;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./Card.css"
 
 
-const Card=({Hotellist})=>{
-    const{name}=Hotellist;
-    console.log( "ATIF",Hotellist);
+const Card=({name,cuisines,rating,Price,img})=>{
+    // const{name,cuisines,rating,Price,img}=Hotel;
+  
     return(
       <div className="card ">
-        <img  className="cardimg" src="https://upload.wikimedia.org/wikipedia/commons/5/5a/%22Hyderabadi_Dum_Biryani%22.jpg"/>
+        <img  className="cardimg" src={img}/>
         <h1>{name}</h1>
-        
+        <h2>{cuisines}</h2>
+        <h2>{Price}</h2>
+        <h2>{rating} star</h2>
       </div>
     );
 }
